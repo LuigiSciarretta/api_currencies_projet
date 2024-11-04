f6231941a98489ceffc4d3d10ad8e5642301d283
LuigiSciarretta <82830074+LuigiSciarretta@users.noreply.github.com>
2024-11-04 17:59:33 +0100

    fix main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def prepare_data_for_db(json_response:dict) -> list[tuple]:
     The goal of this function is to prepare data in the record format for the DB insert"""
     processing_data = [] 
     base_currency = json_response['base']
-    for daily_date, corresponding_curr in storic_result['rates'].items():
+    for daily_date, corresponding_curr in json_response['rates'].items():
         for corresponding_currency, corresponding_value in corresponding_curr.items():
             processing_data.append((base_currency, daily_date, corresponding_currency, corresponding_value))
     return processing_data
@@ -17,20 +17,25 @@ def prepare_data_for_db(json_response:dict) -> list[tuple]:
 if __name__ == '__main__':
     
     #base_url = "https://api.frankfurter.app/"
+    # currency_list = [
+    # 'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'GBP', 'HKD',
+    # 'HUF', 'IDR', 'ILS', 'INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK',
+    # 'NZD', 'PHP', 'PLN', 'RON', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR']
+
     client = APIClient()
-    storic_result = client.get_storic_rates('2024-01-01', '2024-05-05')
+    storic_result = client.get_storic_rates('2002-01-01', '2024-11-04')
+
     data = prepare_data_for_db(storic_result)
+    table_name = 'storic_currencies_table_official'
 
     #database iteraction
     db = Database()
-    db_path = os.path.join(os.path.dirname(__file__), 'data', 'currencyDB.db') #, 'db_prova.db'
-    print(f"db path {db_path}")
+    db_path = os.path.join(os.path.dirname(__file__), 'data', 'currency.db') #, 'db_prova.db'
+    # #print(f"db path {db_path}")
     db.create_connection(db_path)
-    db.create_table()
+    db.create_table(table_name)
     db.test_table_creation()
-    db.insert_values(data)
-    db.test_insert()
+    db.insert_values(data, table_name)
+    #db.test_insert(table_name)
+    db.count_row(table_name)
     db.close_connection()
-    
-
-    
